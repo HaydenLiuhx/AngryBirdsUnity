c1de607f364e84ec346d2fc6f77a1b4d2d809c29
HaydenLiu <huangxunliu@gmail.com>
2020-07-20 09:08:36 +1000

    gameadd

---
+++ Assets/Bird.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    bool OnClicked = false;
+
+    public Transform CenterPos;
+    public float MaxDistance = 2f;
+
+    SpringJoint2D spj;
+
+    public float ReleaseTime = 0.15f;
+
+    Rigidbody2D rg;
+
+    public LineRenderer FrontLine;
+    public LineRenderer BackLine;
+
+    public Transform FrontBond;
+    public Transform BackBond;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spj = GetComponent<SpringJoint2D>();
+        rg = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OnClicked)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+
+            FrontLine.SetPosition(0, FrontBond.position);
+            FrontLine.SetPosition(1, transform.position);
+            BackLine.SetPosition(0, BackBond.position);
+            BackLine.SetPosition(1, transform.position);
+
+            if (Vector3.Distance(transform.position,CenterPos.position) > MaxDistance)
+            {
+                Vector3 direction = (transform.position - CenterPos.position).normalized * MaxDistance;
+                transform.position = CenterPos.position + direction;
+                FrontLine.SetPosition(0, FrontBond.position);
+                FrontLine.SetPosition(1, transform.position);
+                BackLine.SetPosition(0, BackBond.position);
+                BackLine.SetPosition(1, transform.position);
+            }
+        }
+    }
+    private void OnMouseDown()
+    {
+        OnClicked = true;
+        rg.isKinematic = true;
+    }
+     
+    private void OnMouseUp()
+    {
+        OnClicked = false;
+        rg.isKinematic = false;
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(ReleaseTime);
+        spj.enabled = false;
+        FrontLine.enabled = false;
+        BackLine.enabled = false;
+    }
+}
---
+++ Assets/Pig.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pig : MonoBehaviour
+{
+    public float MaxSpeed = 5f;
+
+    public GameObject DeadEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.relativeVelocity.magnitude);
+        if(collision.relativeVelocity.magnitude > MaxSpeed)
+        {
+            Instantiate(DeadEffect,transform.transform.position,transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+}
